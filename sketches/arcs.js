d4452cce8425b211ae40c56a8401f9f9274fb9c1
Alex Louden <alex@louden.com>
2020-11-11 21:03:07 +0800

    working

--- sketches/arcs.js
+++ sketches/arcs.js
@@ -10,7 +10,7 @@ const gridSize = 50
 const isogrid = ({ context, width, height }) => {
   let ar = []
   const aspect = width / height
-  console.log({ aspect })
+  // console.log({ aspect })
   for (let y = 0; y < gridSize / aspect - 1; y++) {
     for (let x = 0; x < gridSize; x++) {
       const yOffset = x % 2 ? 0 : 0.5
@@ -22,9 +22,9 @@ const isogrid = ({ context, width, height }) => {
       ])
     }
   }
-  context.fillStyle = 'rgb(50, 50, 50)'
+  context.fillStyle = 'rgb(150, 150, 150)'
   ar.map(([x, y]) => context.fillRect(x - 1, y - 1, 2, 2))
-  console.log({ ar })
+  // console.log({ ar })
   return ar
 }
 
@@ -39,40 +39,93 @@ const getNeighbours = (i, grid) => {
   const x = i % gridSize
   const y = Math.floor(i / gridSize)
   const yGridSize = grid.length / gridSize - 1
-  console.log({ i, grid, offset, x, y })
+  // console.log({ i, grid, offset, x, y })
 
   let points = []
   // top left
-  if (x > 0 && y >= offset) points.push(grid[i - 1 - gridSize * offset])
+  if (x > 0 && y >= offset) points.push(i - 1 - gridSize * offset)
   // top
-  if (x >= 0 && y > 0) points.push(grid[i - gridSize])
+  if (x >= 0 && y > 0) points.push(i - gridSize)
   // top right
-  if (x < gridSize - offset && y >= offset) points.push(grid[i + 1 - gridSize * offset])
+  if (x < gridSize - offset && y >= offset) points.push(i + 1 - gridSize * offset)
   // bottom left
-  if (x > offset && y < yGridSize + offset) points.push(grid[i - 1 + gridSize * (1 - offset)])
+  if (x > 0 && y < yGridSize + offset) points.push(i - 1 + gridSize * (1 - offset))
   // bottom
-  if (x >= 0 && y < yGridSize) points.push(grid[i + gridSize])
+  if (x >= 0 && y < yGridSize) points.push(i + gridSize)
   // bottom right
-  if (x < gridSize - offset && y < yGridSize + offset)
-    points.push(grid[i + 1 + gridSize * (1 - offset)])
+  if (x < gridSize - offset && y < yGridSize + offset) points.push(i + 1 + gridSize * (1 - offset))
 
   return points
 }
 
+const getFreeNeighbour = ({ visited, pointIndex, grid }) => {
+  const neighbours = random.shuffle(getNeighbours(pointIndex, grid))
+  // console.log({ neighbours })
+
+  for (const neighbour of neighbours) {
+    if (!visited.has(neighbour)) return neighbour
+  }
+  return null
+}
+
+const drawLine = (context, grid, p1, p2) => {
+  context.beginPath()
+  const [x1, y1] = grid[p1]
+  context.moveTo(x1, y1)
+  const [x2, y2] = grid[p2]
+  context.lineTo(x2, y2)
+  context.stroke()
+}
+
 const walkGrid = ({ context, grid }) => {
-  // let pointIndex = random.rangeFloor(grid.length)
-  let pointIndex = 1800
-  const neighbours = getNeighbours(pointIndex, grid)
+  let pointIndex = random.rangeFloor(grid.length)
+  let nextPointIndex = 0
+  let visited = new Set()
+  let available = new Set([...Array(grid.length).keys()])
 
-  context.strokeStyle = 'rgb(255, 0, 0)'
-  const [x, y] = grid[pointIndex]
+  context.strokeStyle = 'rgb(50, 50, 50)'
+
+  let stuck = 0
+
+  while (true) {
+    if (stuck > 100) return
+
+    if (visited.has(pointIndex)) {
+      // already been here - jump somewhere new
+      pointIndex = random.pick(Array.from(available.values()))
+      continue
+    }
+
+    // pick a random neighbour
+    nextPointIndex = getFreeNeighbour({ visited, pointIndex, grid })
+    if (nextPointIndex == null) {
+      // already visited all my neighbours - jump somewhere new
+      pointIndex = random.pick(Array.from(available.values()))
+      stuck++
+      continue
+    }
+
+    // draw a line between them
+    drawLine(context, grid, pointIndex, nextPointIndex)
+    // console.log({ pointIndex, nextPointIndex })
+
+    // keep track of where we've been
+    visited.add(pointIndex)
+    available.delete(pointIndex)
+
+    pointIndex = nextPointIndex
+    stuck = 0
+  }
+
+  // context.strokeStyle = 'rgb(255, 0, 0)'
+  // const [x, y] = grid[pointIndex]
   console.log({ pointIndex, neighbours })
-  drawCircle(context, x, y, 5)
+  // drawCircle(context, x, y, 5)
 
-  context.strokeStyle = 'rgb(0, 0, 255)'
-  neighbours.map(([x, y]) => {
-    drawCircle(context, x, y, 5)
-  })
+  // context.strokeStyle = 'rgb(0, 0, 255)'
+  // neighbours.map(([x, y]) => {
+  //   drawCircle(context, x, y, 5)
+  // })
 }
 
 const sketch = () => {
